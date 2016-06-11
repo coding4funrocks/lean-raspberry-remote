8e41e5469054fe88e0bb31913738da60279d4b03
Monika Kiss <moniq@outlook.com>
2016-06-11 20:00:04 +0200

    initial commit

---
+++ app/API/Client/ErrorModel.js
@@ -0,0 +1,19 @@
+goog.provide('API.Client.errorModel');
+
+/**
+ * @record
+ */
+API.Client.ErrorModel = function() {}
+
+/**
+ * @type {!number}
+ * @export
+ */
+API.Client.ErrorModel.prototype.code;
+
+/**
+ * @type {!string}
+ * @export
+ */
+API.Client.ErrorModel.prototype.message;
+
---
+++ app/API/Client/NewDevice.js
@@ -0,0 +1,19 @@
+goog.provide('API.Client.newDevice');
+
+/**
+ * @record
+ */
+API.Client.NewDevice = function() {}
+
+/**
+ * @type {!number}
+ * @export
+ */
+API.Client.NewDevice.prototype.id;
+
+/**
+ * @type {!string}
+ * @export
+ */
+API.Client.NewDevice.prototype.name;
+
